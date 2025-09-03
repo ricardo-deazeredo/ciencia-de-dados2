7eeb75ee9f02b7b927077b4a4ed3fa4fac365c42
ricardo-deazeredo <ricardo.azeredoaz@gmail.com>
2025-09-03 19:32:33 +0000

    aula 6

--- exercícios/aula06.py
+++ exercícios/aula06.py
@@ -5,5 +5,10 @@ import numpy as np
 np.random.seed(42)
 
 dados = pd.DataFrame({
-    "Orcamento campanha": np.random.randint(1000, 5000, size=100)
+    "Orcamento campanha": np.random.randint(1000, 5000, size=100),
+    "Visualizacoes anuncio": np.random.randint(2000, 20000, size= 100),
+    "Vendas": np.random.randint(10, 1000, size=100),
+    "Cliques": np.random.randint(100, 2000, size=100)
 })
+
+print(dados.corr().round(4))
